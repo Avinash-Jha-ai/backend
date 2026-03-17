9a15e9dd89e82b24c1ab8579cb563ce75c3840e7
Avinash Jha <avinashjha9803a@gmail.com>
2026-03-17 15:15:40 +0530

    public

--- src/app.js
+++ src/app.js
@@ -6,10 +6,14 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+
+
 app.use(cors({
     origin:"http://localhost:5173",
     credentials:true
 }))
+
+app.use(express.static("./public"))
 /**
  * Routes
 */
